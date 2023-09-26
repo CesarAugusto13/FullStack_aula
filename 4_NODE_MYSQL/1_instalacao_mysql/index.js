3d46ec968fed3ca85ff8113ad950a15be63ae28c
César Augusto <cesiha13@gmail.com>
2023-09-26 20:21:51 -0300

    Introdução ao mysql

---
+++ 4_NODE_MYSQL/1_instalacao_mysql/index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+const mysql = require("mysql")
+
+const app = express()
+
+const conn = mysql.createConnection({   
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "nodemysql",
+    port: 3307
+})
+
+conn.connect
